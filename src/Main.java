413c2593126d65ff6fe744aebf61f484612c4692
yourkeksuk <igor-18css@mail.ru>
2022-07-17 22:42:08 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int start = 300;     // Начальный счет
+        int pay = 1500;      // Сумма пополнения
+        int discount = pay / 100;    // Сумма кэшбека
+
+        if (pay > 1000){
+            System.out.println("Скидка предоставляется в размере " + (discount) + " рублей");
+        }
+        System.out.println("Итоговая сумма счета: " + ((start) + (pay) + (discount)) + " рубля(ей)");
+    }
+}
